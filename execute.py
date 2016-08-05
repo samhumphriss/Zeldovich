5b4abea2e2d141a728c424cb8a1628fab86999a9
Sam Humphriss (NMcC) <rhgk18@xgpc31.phyast.dur.ac.uk>
2016-08-05 16:13:45 +0100

    Investigated the properties of the BAO pk vs the Non-BAO pk

---
+++ execute.py
@@ -0,0 +1,17 @@
+import zeldovich_init as ic
+import numpy as np
+import cic_dens_wrapper as cdw
+
+def run_dens(pk, redshift=0, boxsize=512, ngrid=128, trand=True, seed=314159):
+    pos  = ic.main(redshift, pk, boxsize, ngrid, trand, seed)
+    densr = cdw.get_dens(pos[0],pos[1],pos[2], ngrid, boxsize)
+    densz = cdw.get_dens(pos[0],pos[1],pos[3], ngrid, boxsize)
+    dens = np.swapaxes(densr, 2, 0)
+    
+    return dens
+    
+def run_pos(pk, redshift=0, boxsize=512, ngrid=128, trand=True, seed=314159):
+    pos  = ic.main(redshift, pk, boxsize, ngrid, trand, seed)
+    
+    return pos
+
