e67233030f1487dbbfcac863a273bce109b2f033
Sam Humphriss <rhgk18@cosma-e.cosma>
2016-08-08 20:59:32 +0100

    Folder allocation now handled within the script shell.

--- queue_script_cord_bao.sh
+++ queue_script_cord_bao.sh
@@ -2,15 +2,20 @@
 #BSUB -L /bin/bash
 #BSUB -q cordelia
 #BSUB -n 1 #Number of processors
-#BSUB -J zeldovich_stats_bao[1-300] #Job name
+#BSUB -J zeldovich_stats_bao[1-15] #Job name
 #BSUB -eo ./dump/zeldovich_stats_bao%I.err #Error outputs
 #BSUB -oo ./dump/zeldovich_stats_bao%I.out #Program outputs
 #BSUB -P durham
 #BSUB -R "span[hosts=1]" #Processors per node
 #BSUB -W 02:00 #Wall clock time
 
+folderloc="/gpfs/data/rhgk18/results/Troubleshoot"
+
+mkdir $folderloc
+mkdir "$folderloc/bao"
+
 . /etc/profile.d/modules.sh
 module purge
 module load python/2.7.3
 
-python exeMAIN.py -b 512 -g 256 -r -f "/B512_G256_003/bao" -p "wig.txt" --runindex ${LSB_JOBINDEX}
+python exeMAIN.py -b 512 -g 256 -r -f "$folderloc/bao" -p "wig.txt" --runindex ${LSB_JOBINDEX}
--- queue_script_cord_nobao.sh
+++ queue_script_cord_nobao.sh
@@ -9,8 +9,13 @@
 #BSUB -R "span[hosts=1]" #Processors per node
 #BSUB -W 02:00 #Wall clock time
 
+folderloc="/gpfs/data/rhgk18/results/Troubleshoot"
+
+mkdir $folderloc
+mkdir "$folderloc/nobao"
+
 . /etc/profile.d/modules.sh
 module purge
 module load python/2.7.3
 
-python exeMAIN.py -b 512 -g 256 -r -f "/B512_G256_003/nobao" -p "nowig.txt" --runindex ${LSB_JOBINDEX}
+python exeMAIN.py -b 512 -g 256 -r -f "$folderloc/nobao" -p "nowig.txt" --runindex ${LSB_JOBINDEX}
