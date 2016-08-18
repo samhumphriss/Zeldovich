d4f1859471ba0dd7bee72f28b7a3bdec84e82b3d
Sam Humphriss (NMcC) <rhgk18@xgpc31.phyast.dur.ac.uk>
2016-08-18 12:20:54 +0100

    Updated spatial_stats to improve performance.

--- spatial_stats.py
+++ spatial_stats.py
@@ -2,10 +2,6 @@ import numpy as N
 
 sqrt2 = N.sqrt(2.)
 
-# density is the configuration-space density grid (real-valued, shape = ngrid x ngrid x ngrid)
-# nkbins is the number of bins to compute the power spectrum in. It computes it in log-spaced bins in the range (2*Pi/L to Pi*ngrid / L)
-# deconvolve_cic if you want to deconvolve a cloud-in-cell window function
-# exp_smooth is the size of an exponential smoothing window to deconvolve (if = 0.0, then it does nothing)
 def getPk(density, nkbins=40, boxsize=512., deconvolve_cic=False, exp_smooth=0.0):
     #make sure the density has mean 0
     density=density-N.mean(density)
@@ -90,12 +86,12 @@ def getXi(density, nrbins=40, boxsize=512., get2d=False, deconvolve_cic=False, e
     
     
     #Fourier transform of density
-    deltak=N.fft.rfftn(density)
-    sk=deltak.shape
+    density=N.fft.rfftn(density)
+    sk=density.shape
     
     #Square the density in Fourier space to get the 3D power, make sure k=0 mode is 0
-    dk2=(deltak*N.conjugate(deltak)).astype(N.float)
-    dk2[0,0,0]=0.0
+    density=(density*N.conjugate(density)).astype(N.float)
+    density[0,0,0]=0.0
     
     #set up k-grid
     kmin=2*N.pi/boxsize
@@ -103,37 +99,34 @@ def getXi(density, nrbins=40, boxsize=512., get2d=False, deconvolve_cic=False, e
     
     a = N.fromfunction(lambda x,y,z:x, sk).astype(N.float)
     a[N.where(a > ngrid/2)] -= ngrid
-    b = N.fromfunction(lambda x,y,z:y, sk).astype(N.float)
-    b[N.where(b > ngrid/2)] -= ngrid
+
+    b=N.transpose(a, (1, 0, 2))
+    #b = N.fromfunction(lambda x,y,z:y, sk).astype(N.float)
+    #b[N.where(b > ngrid/2)] -= ngrid
     c = N.fromfunction(lambda x,y,z:z, sk).astype(N.float)
     c[N.where(c > ngrid/2)] -= ngrid
     kgrid = kmin*N.sqrt(a**2+b**2+c**2).astype(N.float)
     if (deconvolve_cic):
-        wx=N.sin(kmin*a*N.pi/(2*kny))**4/(kmin*a*N.pi/(2*kny))**4
-        wx[N.where(a==0)]=1.0
-        wy=N.sin(kmin*b*N.pi/(2*kny))**4/(kmin*b*N.pi/(2*kny))**4
-        wy[N.where(b==0)]=1.0
-        wz=N.sin(kmin*c*N.pi/(2*kny))**4/(kmin*c*N.pi/(2*kny))**4
-        wz[N.where(c==0)]=1.0
-        ww=wx*wy*wz
-        dk2=dk2/ww
+
+        ww=N.sin(kmin*a*N.pi/(2*kny))**4/(kmin*a*N.pi/(2*kny))**4*N.sin(kmin*b*N.pi/(2*kny))**4/(kmin*b*N.pi/(2*kny))**4*N.sin(kmin*c*N.pi/(2*kny))**4/(kmin*c*N.pi/(2*kny))**4
+	ww[N.where((a==0) | (b==0) | (c==0))]=1.0
+
+        density=density/ww
         
     if (exp_smooth!=0):
-        filt=N.exp(-kgrid**2*exp_smooth**2)
-        dk2=dk2/filt
+        dk2=dk2/N.exp(-kgrid**2*exp_smooth**2)
         
     #Fourier transform back to config space
-    xi3d = N.fft.irfftn(dk2)/ngrid**3
+    density = N.fft.irfftn(density)/ngrid**3
     
     #define coordinates in config space
     
     rmin=boxsize/N.float(ngrid)
     a = N.fromfunction(lambda x,y,z:x, (ngrid, ngrid, ngrid))
     a[N.where(a > ngrid/2)] -= ngrid
-    b = N.fromfunction(lambda x,y,z:y, (ngrid, ngrid, ngrid))
-    b[N.where(b > ngrid/2)] -= ngrid
-    c = N.fromfunction(lambda x,y,z:z, (ngrid, ngrid, ngrid))
-    c[N.where(c > ngrid/2)] -= ngrid
+
+    b=N.transpose(a, (1, 0, 2))
+    c=N.transpose(a, (2, 1, 0))
     
     
     #compute 2d correlation function, xi(rp, pi)
@@ -156,7 +149,7 @@ def getXi(density, nrbins=40, boxsize=512., get2d=False, deconvolve_cic=False, e
             rzmean[i]=N.sum(c0_ind[cutsz[i]:cutsz[i+1]]*rz[cutsz[i]:cutsz[i+1]])/N.sum(c0_ind[cutsz[i]:cutsz[i+1]])
             index = N.argsort(rp[i,:,:].flatten())
             rp_sort = rp[i,:,:].flatten()[index]
-            xi3d_sort = xi3d[i,:,:].flatten()[index]
+            xi3d_sort = density[i,:,:].flatten()[index]
             c0_indexed = c0[i,:,:].flatten()[index]
             cuts = N.searchsorted(rp_sort,binedges)
             numinbin = N.zeros_like(binedges)
@@ -182,11 +175,11 @@ def getXi(density, nrbins=40, boxsize=512., get2d=False, deconvolve_cic=False, e
         rmean=N.zeros_like(binedges)
         
         rgrid=rgrid.flatten()
-        xi3d = xi3d.flatten()
+        density = density.flatten()
         index = N.argsort(rgrid)
         
         rgrid=rgrid[index]
-        xi3d=xi3d[index]
+        density=density[index]
         c0=0.*c.flatten()+1.
         c0[N.where(c.flatten()==0.)]-=0.5
         c0=c0[index]
@@ -196,7 +189,7 @@ def getXi(density, nrbins=40, boxsize=512., get2d=False, deconvolve_cic=False, e
         for i in N.arange(0, nrbins-1):
             if (cuts[i+1]>cuts[i]):
                 numinbin[i]=N.sum(c0[cuts[i]:cuts[i+1]])
-                xir[i]=N.sum(c0[cuts[i]:cuts[i+1]]*xi3d[cuts[i]:cuts[i+1]])
+                xir[i]=N.sum(c0[cuts[i]:cuts[i+1]]*density[cuts[i]:cuts[i+1]])
                 rmean[i]=N.sum(c0[cuts[i]:cuts[i+1]]*rgrid[cuts[i]:cuts[i+1]])
         wn0=N.where(numinbin>0.)
         xir=xir[wn0]
